354d486bb1793a194fa1b564cba217abf6f7c631
Chelsea Li <148104000+Chelsealily@users.noreply.github.com>
2024-01-04 15:46:36 +0000

    formatted docs

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,58 +1,74 @@
-import {FormEvent, MouseEvent, useState } from 'react'
-import './App.scss'
-import TaskItem from './TaskItem/TaskItem';
+import { FormEvent, MouseEvent, useState } from "react";
+import "./App.scss";
+import TaskItem from "./TaskItem/TaskItem";
 
 function App() {
   const [todos, setTodos] = useState<string[]>([]);
   const [inputTodo, setInputTodo] = useState<string>("");
   const [checked, setChecked] = useState<boolean>(false);
 
-  const reset = () => window.location.reload()
+  const reset = () => window.location.reload();
 
   const handleInput = (event: FormEvent<HTMLInputElement>) => {
-    setInputTodo(event.currentTarget.value)
-  }
+    setInputTodo(event.currentTarget.value);
+  };
 
-   const handleSubmit= (event:MouseEvent<HTMLButtonElement>) => {
+  const handleSubmit = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    setTodos([...todos, inputTodo])
-    setInputTodo("")
-  }
+    setTodos([...todos, inputTodo]);
+    setInputTodo("");
+  };
 
-  const handleDelete = (index:any ) => {
-    let newTodos = [...todos]
-    newTodos.splice(index, 1)
-    setTodos(newTodos)
-  }
+  const handleDelete = (index: any) => {
+    let newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
 
-    const handleChange= () => {
-        setChecked(!checked);
-    }
-
-  
+  const handleChange = () => {
+    setChecked(!checked);
+  };
 
   return (
-    
-      <section className="todo-container">
+    <section className="todo-container">
       <div className="todo">
-        <button className="todo__button--reset" onClick={reset}> Reset </button>
-      <h1>✏️ My Todo List</h1>
-      
-      <div className="todo__input">
-      <form id="todo__form">
-        <input id="todo__input" type="text" placeholder="🔎 Add task.." value={inputTodo} onChange={handleInput}/>
-        <button className="todo__button" onClick={handleSubmit}> ➕ </button>
-      </form></div> 
-      <div className="todo__output">
-      <ul>
-        {todos.map((task, index) => (
-          <TaskItem key={index} taskname = {task} index = {index} handleDelete={handleDelete} handleChange={handleChange}/>
+        <button className="todo__button--reset" onClick={reset}>
+          {" "}
+          Reset{" "}
+        </button>
+        <h1>✏️ My Todo List</h1>
+
+        <div className="todo__input">
+          <form id="todo__form">
+            <input
+              id="todo__input"
+              type="text"
+              placeholder="🔎 Add task.."
+              value={inputTodo}
+              onChange={handleInput}
+            />
+            <button className="todo__button" onClick={handleSubmit}>
+              {" "}
+              ➕{" "}
+            </button>
+          </form>
+        </div>
+        <div className="todo__output">
+          <ul>
+            {todos.map((task, index) => (
+              <TaskItem
+                key={index}
+                taskname={task}
+                index={index}
+                handleDelete={handleDelete}
+                handleChange={handleChange}
+              />
             ))}
-      </ul>    
-      </div></div>
-</section>
-      
-  )
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
--- todo-list/src/TaskItem/TaskItem.tsx
+++ todo-list/src/TaskItem/TaskItem.tsx
@@ -1,25 +1,30 @@
 import { ChangeEventHandler, MouseEventHandler } from "react";
-import "./TaskItem.scss"
+import "./TaskItem.scss";
 
 type TaskItemProps = {
-        taskname:string;
-        index:any;
-        handleDelete:MouseEventHandler<HTMLButtonElement>;
-        handleChange:ChangeEventHandler<HTMLInputElement>;
-    }
-   
-const TaskItem = ({taskname, index, handleDelete, handleChange}:TaskItemProps) => {
-    
+  taskname: string;
+  index: any;
+  handleDelete: MouseEventHandler<HTMLButtonElement>;
+  handleChange: ChangeEventHandler<HTMLInputElement>;
+};
 
+const TaskItem = ({
+  taskname,
+  index,
+  handleDelete,
+  handleChange,
+}: TaskItemProps) => {
   return (
-    <div className="todo-item" >
-        <li key={index}>
-         <input type="checkbox" onChange={() => handleChange(index)}></input>  
-          {taskname}
-          <button className="todo__button" onClick={() => handleDelete(index)}>🗑️</button>
-          </li>
-      </div>
-  )
-}
- 
-export default TaskItem
+    <div className="todo-item">
+      <li key={index}>
+        <input type="checkbox" onChange={() => handleChange(index)}></input>
+        {taskname}
+        <button className="todo__button" onClick={() => handleDelete(index)}>
+          🗑️
+        </button>
+      </li>
+    </div>
+  );
+};
+
+export default TaskItem;
