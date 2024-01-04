227e8a5239b140b8e9e9ecccde8e060526aede8d
Chelsea Li <148104000+Chelsealily@users.noreply.github.com>
2024-01-04 12:31:40 +0000

    minor stylings and majority of layouts done. need to work on tick and bin functions

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,29 +1,57 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
+import {FormEvent, MouseEvent, useState } from 'react'
 import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState<string[]>([]);
+  const [inputTodo, setInputTodo] = useState<string>("");
+
+  const reset = () => window.location.reload()
+
+  const handleInput = (event: FormEvent<HTMLInputElement>) => {
+    setInputTodo(event.currentTarget.value)
+  }
+
+   const handleSubmit= (event:MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setTodos([...todos, inputTodo])
+    setInputTodo("")
+  }
+
+  //need to fix, deletes lowest 1
+ const handleDelete = (index:number, _event:MouseEvent<HTMLButtonElement>) => {
+    let newTodos = [...todos]
+    newTodos.splice(index, 1)
+    console.log(index)
+    setTodos(newTodos)
+  }
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        
-        </a>
-      </div>
-      <h1>To do List</h1>
+    
+      <section className="todo-container">
       <div className="todo">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-         <p className="todo__text">
-       HIHI
-      </p>
-      </div>
-     
-    </>
+        <button className="todo__button--reset" onClick={reset}> Reset </button>
+      <h1>✏️ My Todo List</h1>
+      
+      <div className="todo__input">
+      <form>
+        <input type="text" placeholder="🔎 Add task.." value={inputTodo} onChange={handleInput}/>
+        <button className="todo__button" onClick={handleSubmit}> ➕ </button>
+      </form></div> 
+      <div className="todo__output">
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo}>
+         <button className="todo__button">✅</button>   
+          {todo}
+          <button className="todo__button" onClick={handleDelete}>🗑️</button>
+          </li>
+        ))}
+      </ul>
+      </div></div>
+</section>
+      
   )
 }
 
